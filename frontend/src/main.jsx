9cd758326bf93f412a088d87de28398677d8c8f6
nikghost17 <vnikhil1705@gmail.com>
2025-01-23 22:46:56 +0530

    login page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
 import './index.css'
 import App from './App.jsx'
 
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Home/>
+  },
+  {
+    path:"/login",
+    element:<Login/>
+  }
+])
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router = {router}/>
   </StrictMode>,
 )
